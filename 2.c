cb28483ef3d0637c1109b5adb601dcfe843c9df0
Ivan1python <128496244+Ivan1python@users.noreply.github.com>
2023-03-22 08:21:25 -0600

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,25 @@
+//Programa bien pendejo
+/* Programa hecho porn un pendejo */
+#include<stdio.h>
+#INCLUDE<conio.h>
+int main()
+{
+	int OP, T;
+	printf("Introduce la opción que deseas");
+	scanf("%d ",&OP);
+	printf(" Introduce la opción que deseas");
+	sacnf("%d",&T);
+	if(OP==1){
+		Res=(float)T/5;
+	}
+	else if(OP==2)	{
+	 RES=pow(T,T);
+	}
+     else if(OP==3 || OP==4){
+     	RES=6*(float)T/2
+	 }
+     else {
+	 RES=1}
+    	printf("El resultado es");
+    	return 0;
+	 }
